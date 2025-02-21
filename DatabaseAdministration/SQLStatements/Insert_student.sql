25f681bf56ab15cf2b4129d19cff443eeddbef2d
Karabo Tsaoane <karabo.tsaoane@sambeconsulting.com>
2025-02-21 10:04:32 +0200

    Updated Insert_student.sql script

--- DatabaseAdministration/SQLStatements/Insert_student.sql
+++ DatabaseAdministration/SQLStatements/Insert_student.sql
@@ -1,3 +1,16 @@
 SET IDENTITY_INSERT Students ON;
 INSERT INTO Students (StudentID , FirstName , LastName , Email)
-VALUES(2236712 , 'lebo' , 'Motaung', 'lebo@gmail.com');
+VALUES
+    (1264156, 'John', 'Doe', 'john.doe@gmail.com'),  
+    (2254793, 'Jane', 'Smith', 'jane.smith@gmail.com'),  
+    (3147852, 'Alice', 'Brown', 'alice.brown@gmail.com'),
+	(1458745, 'Karabo', 'Motloung', 'karabo@gmail.com'),  
+    (2548962, 'Jane', 'Smith', 'jane.smith@gmail.com'),  
+    (3254792, 'Mickey', 'Daniels', 'mic.daniels@gmail.com'),
+	(4569874, 'Michael', 'Johnson', 'michael.johnson@gmail.com'), 
+    (5845624, 'Emily', 'Davis', 'emily.davis@example.com'),  
+    (6896541, 'Chris', 'Wilson', 'chris.wilson@gmail.com'),  
+    (7125489, 'Sarah', 'Miller', 'sarah.miller@gmail.com'),  
+    (8145698, 'David', 'Anderson', 'david.anderson@gmail.com'),  
+    (9785423, 'Emma', 'Thomas', 'emma.thomas@gmail.com'),  
+    (104568, 'James', 'Moore', 'james.moore@gmail.com');  
