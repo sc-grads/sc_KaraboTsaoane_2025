a85111a8dc0b3975a0e4dcb821460fc50b68cca5
Karabo Tsaoane <karabo.tsaoane@sambeconsulting.com>
2025-03-11 11:33:50 +0200

    Added Insert_PersonInfo.sql

---
+++ DatabaseAdministration/SQLStatements/Insert_PersonInfo.sql
@@ -0,0 +1,11 @@
+USE FirstDatabase;
+GO
+
+INSERT INTO PersonalInfo (FirstName, LastName, DateOfBirth)
+VALUES 
+    ('John', 'Doe', '1990-05-15'),
+    ('Jane', 'Smith', '1985-10-20'),
+    ('Michael', 'Johnson', '1992-07-08'),
+    ('Emily', 'Davis', '1998-12-03'),
+    ('David', 'Wilson', '2000-01-25');
+GO
