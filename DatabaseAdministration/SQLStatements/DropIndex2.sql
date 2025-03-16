fe7c50a0aabba1b074388c55d9471c9206a8903c
Karabo Tsaoane <karabo.tsaoane@sambeconsulting.com>
2025-03-16 10:27:52 +0200

    Added more scripts on fragmentation - droping and rebuilding

---
+++ DatabaseAdministration/SQLStatements/DropIndex2.sql
@@ -0,0 +1,5 @@
+USE [AdventureWorks2016]
+GO
+/****** Object:  Index [idx_firstname_lastname]    Script Date: 2025/03/16 10:09:09 ******/
+DROP INDEX [idx_firstname_lastname] ON [Person].[Person]
+GO
