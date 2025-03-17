ae209f3de1078b86460b88612b5447e3946a4c12
Karabo Tsaoane <karabo.tsaoane@sambeconsulting.com>
2025-03-17 15:07:52 +0200

    Added TransactionalLog.sql

---
+++ DatabaseAdministration/SQLStatements/TransactionalLog.sql
@@ -0,0 +1,2 @@
+BACKUP LOG [AdventureWorks2016] TO  DISK = N'C:\SQL Backup\AdventureWorks2016_01.tm' WITH NOFORMAT, NOINIT,  NAME = N'AdventureWorks2016-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
