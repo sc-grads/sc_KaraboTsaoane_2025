aa375cf5149eab64cc870964440d98c0f94d2567
Karabo Tsaoane <karabo.tsaoane@sambeconsulting.com>
2025-03-17 14:31:01 +0200

    Added a backup script

---
+++ DatabaseAdministration/SQLStatements/BackupAdventure.sql
@@ -0,0 +1,2 @@
+BACKUP DATABASE [AdventureWorks2016] TO  DISK = N'C:\SQL Backup\AdventureWorks201617032015.bak',  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\AdventureWorks2016' WITH NOFORMAT, NOINIT,  NAME = N'AdventureWorks2016-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
