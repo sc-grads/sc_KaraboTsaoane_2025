174a4e1ce09cdf7d01dea4d01e3b0a82e2baa126
Karabo Tsaoane <karabo.tsaoane@sambeconsulting.com>
2025-03-18 12:56:31 +0200

    Added 4 more scripts on permissions - Grant , Revoke , Deny

---
+++ DatabaseAdministration/SQLStatements/Revoke.sql
@@ -0,0 +1,8 @@
+GRANT UPDATE ON SCHEMA::Sales TO [LAPTOP-OKDI7SQH\Karabo Tsaoane]
+
+EXECUTE AS USER ='LAPTOP-OKDI7SQH\Karabo Tsaoane';
+SELECT *FROM fn_my_permissions ('Sales' ,'SCHEMA')
+REVERT;
+GO
+
+REVOKE CONTROL ON SCHEMA::Sales TO [LAPTOP-OKDI7SQH\Karabo Tsaoane]
---
+++ DatabaseAdministration/SQLStatements/Select_Grant.sql
@@ -0,0 +1,3 @@
+GRANT CONTROL ON SCHEMA::Sales TO [LAPTOP-OKDI7SQH\Karabo Tsaoane]
+
+SELECT *FROM fn_my_permissions ('Sales' ,'SCHEMA')
