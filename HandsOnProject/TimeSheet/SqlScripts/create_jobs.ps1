4982cbf5e65f97a6b279d455eee99e592335465f
Karabo Tsaoane <karabo.tsaoane@sambeconsulting.com>
2025-06-19 14:28:37 +0200

    Modified file

--- HandsOnProject/TimeSheet/SqlScripts/create_jobs.ps1
+++ HandsOnProject/TimeSheet/SqlScripts/create_jobs.ps1
@@ -6,18 +6,20 @@ param (
 
 $jobs = @(
   @{ Name = "Client"; PackagePath = "\SSISDB\MyPackageDemos\TimeSheet\ExtractClientNames2.dtsx" },
-  @{ Name = "Employee";  PackagePath = "\SSISDB\MyPackageDemos\TimeSheet\ExtractEmployeeNames2.dtsx" },
-  @{ Name = "Leave";    PackagePath = "\SSISDB\MyPackageDemos\TimeSheet\ExtractLeave2.dtsx" },
+  @{ Name = "Employee"; PackagePath = "\SSISDB\MyPackageDemos\TimeSheet\ExtractEmployeeNames2.dtsx" },
+  @{ Name = "Leave"; PackagePath = "\SSISDB\MyPackageDemos\TimeSheet\ExtractLeave2.dtsx" },
   @{ Name = "Timesheet"; PackagePath = "\SSISDB\MyPackageDemos\TimeSheet\ExtractTimesheet2.dtsx" }
 )
 
 foreach ($job in $jobs) {
+  $scheduleName = "RunEveryMinuteDaily_$($job.Name)"
+
   $sql = @"
 IF NOT EXISTS (SELECT 1 FROM msdb.dbo.sysjobs WHERE name = N'$($job.Name)')
 BEGIN
     -- Create the job
     EXEC msdb.dbo.sp_add_job @job_name = N'$($job.Name)';
-    
+
     -- Create the job step to run the SSIS package
     EXEC msdb.dbo.sp_add_jobstep
         @job_name = N'$($job.Name)',
@@ -26,28 +28,32 @@ BEGIN
         @command = N'/ISSERVER "$($job.PackagePath)" /SERVER "$SqlServer"',
         @on_success_action = 1,
         @on_fail_action = 2;
+END;
 
-    -- Add the schedule to run every minute daily
-    IF NOT EXISTS (SELECT 1 FROM msdb.dbo.sysschedules WHERE name = N'RunEveryMinuteDaily_$($job.Name)')
-    BEGIN
-        EXEC msdb.dbo.sp_add_schedule
-            @schedule_name = N'RunEveryMinuteDaily_$($job.Name)',
-            @freq_type = 4,                -- Daily
-            @freq_interval = 1,            -- Every 1 day
-            @freq_subday_type = 1,         -- Minutes
-            @freq_subday_interval = 1,     -- Every 1 minute
-            @active_start_time = 000000,
-            @active_end_time = 235959;
-    END
+-- Delete old schedule if exists
+IF EXISTS (SELECT 1 FROM msdb.dbo.sysschedules WHERE name = N'$scheduleName')
+BEGIN
+    EXEC msdb.dbo.sp_detach_schedule @job_name = N'$($job.Name)', @schedule_name = N'$scheduleName';
+    EXEC msdb.dbo.sp_delete_schedule @schedule_name = N'$scheduleName';
+END;
 
-    -- Attach the schedule to the job
-    EXEC msdb.dbo.sp_attach_schedule
-        @job_name = N'$($job.Name)',
-        @schedule_name = N'RunEveryMinuteDaily_$($job.Name)';
+-- Create a new schedule to run every minute from 00:01 to 23:59
+EXEC msdb.dbo.sp_add_schedule
+    @schedule_name = N'$scheduleName',
+    @freq_type = 4,                -- Daily
+    @freq_interval = 1,            -- Every day
+    @freq_subday_type = 1,         -- Every X minutes
+    @freq_subday_interval = 1,     -- Every 1 minute
+    @active_start_time = 000100,   -- Start at 00:01 to avoid 00:00 issue
+    @active_end_time = 235959;
 
-    -- Add the job to the SQL Server Agent
-    EXEC msdb.dbo.sp_add_jobserver @job_name = N'$($job.Name)';
-END
+-- Attach the schedule to the job
+EXEC msdb.dbo.sp_attach_schedule
+    @job_name = N'$($job.Name)',
+    @schedule_name = N'$scheduleName';
+
+-- Assign the job to the server
+EXEC msdb.dbo.sp_add_jobserver @job_name = N'$($job.Name)';
 "@
 
   Invoke-Sqlcmd -ServerInstance $SqlServer -Username $Username -Password $Password -Query $sql
